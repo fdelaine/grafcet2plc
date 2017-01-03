e534f19e1122119913794c1e71810f8c07a1b307
f_delaine <florentin.delaine@gmail.com>
2017-01-03 12:19:32 +0100

    Add class structure for api

--- api.py
+++ api.py
@@ -3,3 +3,18 @@
 
 """api.py"""
 
+
+class Api:
+    name = "None"
+
+
+class Simatic_S7_200(Api):
+
+    name = "Simatic S7-200"
+
+    def get_code(cls, grafcet):
+        code = ''
+
+        return code
+
+    get_code = classmethod(get_code)
